c0fea966b62871808be6a8861ac6471f0257fcf3
Jean R <jereyesal@falabella.cl>
2024-08-03 20:38:45 -0400

    cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,18 @@ import { defineConfig, envField } from "astro/config";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server"
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
+  ,
+  adapter: cloudflare()
 });
